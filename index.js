dba1e34100a41cb4540ebdc36ae474053ec7287a
sachinandan-o5 <sachinandan2121@gmail.com>
2024-05-30 01:44:17 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 import dotenv from "dotenv"
 
-import app from "./app.js";
-import { connectDB } from "./db/index.js";
+import app from "./src/app.js";
+import { connectDB } from "./src/db/index.js";
 
 dotenv.config(
     {
