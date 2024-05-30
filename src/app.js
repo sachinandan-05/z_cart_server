e5aae12bc11a92e1f477e6ffea3957850053a1f9
Sachinandan <112381035+sachinandan-05@users.noreply.github.com>
2024-05-30 06:33:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:process.env.CORS_ORIGIN,
+        origin:"https://e-com-client-td2p.vercel.app/",
         methods:["post","get","delete","update"],
         credentials:true
 
